8898a74ea70039535ed02e2399cf555842f44f7d
杨铭 <webming@bitkan.com>
2021-11-04 17:46:45 +0800

    add new

---
+++ hello-world/js/index.js
@@ -0,0 +1,14 @@
+// const wordList = require("./KaoYanluan_1.json");
+// console.log('123');
+// alert('123');
+const nextBtn = document.getElementById('next');
+
+const title = document.getElementById('title');
+// title.innerText = 'success';
+// function nextBtnFun(){
+//   alert('success');
+// };
+
+nextBtn.addEventListener("click", () => {
+  alert('123');
+});
